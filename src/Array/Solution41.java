bdb146fa9f595717123252bb10793a1e049bf32d
why <why930908@outlook.com>
2019-12-22 16:32:30 +0800

    面试精选

---
+++ src/Array/Solution41.java
@@ -0,0 +1,18 @@
+package Array;
+//面试精选
+public class Solution41 {
+    public int firstMissingPositive(int[] nums) {
+        int min =1;
+        int j =1;
+        while (1) {
+            for (int i = 0; i < nums.length; i++) {
+                if (min == nums[i]) {
+                    min++;break;
+                }
+            }
+            j++;
+            if (min != j) break;
+        }
+        return min;
+    }
+}
