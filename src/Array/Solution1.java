8054512c3deca7b1e1a290ca5b14df00a46597a2
Haoyu Wang <why930908@outlook.com>
2019-12-23 12:24:34 +0800

    null

--- src/Array/Solution1.java
+++ src/Array/Solution1.java
@@ -5,12 +5,16 @@ import java.util.Map;
 
 public class Solution1 {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length;i++){
-            map.put(nums[i],target-nums[i]);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
         }
-        for (int i= 0; i < nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             int comp = target - nums[i];
-            if (map.containsKey(comp) && )
+            if (map.containsKey(comp) && map.get(comp) != i) {
+                return new int[]{i, map.get(comp)};
+            }
+        }
+        throw new IllegalArgumentException("No");
     }
 }
---
+++ src/Array/Solution15.java
@@ -0,0 +1,16 @@
+package Array;
+
+import java.util.List;
+import java.util.Map;
+
+public class Solution15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        for (int i =0; i < nums.length;i++){
+            for (int j = i ; j < nums.length;j++){
+
+            }
+        }
+        throw new IllegalArgumentException("No");
+    }
+}
