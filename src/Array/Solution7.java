96e28a1495a87926bd70f1a4fc858c344d5becc6
Haoyu Wang <why930908@outlook.com>
2019-12-23 13:53:26 +0800

    null

---
+++ src/Array/Solution7.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class Solution7 {
+    public int reverse(int x) {
+        int res =0;
+        while (x!=0){
+            int tmp = x %10;
+            if ((res ==Integer.MAX_VALUE/10 && tmp>7) || res > Integer.MAX_VALUE/10) return 0;
+           // if (res > Integer.MAX_VALUE/10 || (res == Integer.MAX_VALUE / 10 && tmp > 7)) return 0;
+            if ((res == Integer.MIN_VALUE/10&&tmp<-8) || res <Integer.MIN_VALUE/10 ) return 0;
+            res = res *10 +tmp;
+            x/=10;
+        }
+        return res;
+    }
+}
