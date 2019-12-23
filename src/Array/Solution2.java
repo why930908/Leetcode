dd0d3af5966bed04d396328e8679923d7a2d5f1a
Haoyu Wang <why930908@outlook.com>
2019-12-23 12:57:23 +0800

    Listnode +

---
+++ src/Array/Solution2.java
@@ -0,0 +1,27 @@
+package Array;
+
+//Listnode +
+
+public class Solution2 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        int tmp =0;
+        ListNode res= new ListNode(0);
+        ListNode current = res;
+        while (l1!=null || l2!=null){
+            int x1 = l1==null? 0 : l1.val;
+            int x2 = l2==null? 0 : l2.val;
+            current.next = new ListNode((x1+x2+tmp)%10);
+            current = current.next;
+            tmp=(x1+x2+tmp)/10;
+            if (l1.next!=null) l1=l1.next;
+            if (l2.next!=null) l2=l2.next;
+
+        }
+        return res.next;
+    }
+}
---
+++ src/String/Solution227.java
@@ -0,0 +1,10 @@
+package String;
+
+import java.util.Stack;
+
+public class Solution227 {
+    public int calculate(String s) {
+        Stack<Character> stack = new Stack<>();
+        return 0;
+    }
+}
