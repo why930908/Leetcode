c8a02b673cfb2fada6865c3dae09326638b83c6d
Haoyu Wang <why930908@outlook.com>
2020-02-18 20:35:12 +0800

    //递归 List: add(i,val)

--- src/Tree/Solution103.java
+++ src/Tree/Solution103.java
@@ -3,43 +3,34 @@ package Tree;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-//乱了乱了 有问题
+//递归 List: add(i,val)
 public class Solution103 {
-    public class TreeNode{
+    public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode(int x){val=x;}
-    }
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root){
-        List<List<Integer>> res = new LinkedList<List<Integer>>();
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        if (root == null) return res;
-        queue.offer(root);
-        int level = 0;
-        while (!queue.isEmpty()){
-            res.add(new LinkedList<>());
-            int size = queue.size();
-            if (level%2==0)
-            {
-                for (int i = 0; i < size;i++){
-                TreeNode node = queue.removeFirst();
-                res.get(level).add(node.val);
-                 if (node.left!=null) queue.offer(node.left);
-                 if (node.right!=null) queue.offer(node.right);
-                }
-            }
-                else {
-                    for (int i = 0;i <size;i++) {
-                        TreeNode node = queue.removeLast();
-                        res.get(level).add(node.val);
-                        if (node.right!=null) queue.offer(node.right);
-                        if (node.left!=null) queue.offer(node.left);
-                    }
-                }
-                level++;
-            }
-        return res;
+
+        TreeNode(int x) {
+            val = x;
         }
     }
+
+    List<List<Integer>> res = new LinkedList<List<Integer>>();
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) return res;
+        helper(root,0);
+        return res;
+    }
+    public void helper(TreeNode root,int level){
+        if (res.size() >= level){
+            res.add(new LinkedList<Integer>());
+        }
+        if (level%2==0){
+            res.get(level).add(root.val);
+        }else res.get(level).add(0,root.val);
+        if (root.left!=null) helper(root.left,level+1);
+        if (root.right!=null) helper(root.right,level+1);
+    }
+}
 
