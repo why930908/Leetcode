0a5650cf8753cee5cc1dc7f8df7a2543a6f107dc
why <why930908@outlook.com>
2020-03-01 16:01:04 +0800

    // 用前缀和遍历路径，目前没太懂

--- src/Tree/Solution559.java
+++ src/Tree/Solution559.java
@@ -1,22 +1,29 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution559 {
+    class Node{
+        public int val;
+        public List<Node> children;
+        public Node(int _val){ val = _val;}
+    }
     private int max = 1,cur =1;
     public int maxDepth(Node root) {
         if (root == null) return 0;
-        helper(root,max,cur);
+        helper(root,1,1);
         return max;
     }
-    public int helper(Node root,int max,int cur){
-        if (root.children == null){
-            max=Math.max(max,cur);
+    public void helper(Node root,int max,int cur){
+        if (root.children.isEmpty()){
+            max = Math.max(max,cur);
         }else {
             for (Node tmp : root.children){
-                cur++;
-                helper(tmp,max,cur);
+                helper(tmp,max,cur+1);
             }
-
         }
-        return max;
     }
+
 }
